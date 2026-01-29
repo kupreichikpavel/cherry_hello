c66ed90bd9f0f088436118fd52e2d0a14a62363a
kupreychikpavel <kupreichik.p@gmail.com>
2026-01-29 10:53:32 +0300

    new info about unable files

--- src/Consts/Consts.java
+++ src/Consts/Consts.java
@@ -1,5 +1,7 @@
 package Consts;
 
+import java.util.List;
+
 public class Consts {
     public static final String ENTER_SOURSE_CRYPT_FILE = "Введите имя файла, который нужно зашифровать";
     public static final String ENTER_DESTINATIOON_FILE = "Введите имя файла, куда сложить результат";
@@ -10,5 +12,26 @@ public class Consts {
         'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
     public static final char[] ALPHABET_ENGLISH ={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+   public static final List<String> UNAVALIBLE_PATHS = List.of(
+            "bootmgr",
+            "Boot\\BCD",
+            "winload.exe",
+            "C:\\Windows\\System32\\ntoskrnl.exe",
+            "C:\\Windows\\System32\\hal.dll",
+            "C:\\Windows\\System32\\win32k.sys",
+            "C:\\Windows\\System32\\kernel32.dll",
+            "C:\\Windows\\System32\\user32.dll",
+            "C:\\Windows\\System32\\gdi32.dll",
+            "C:\\Windows\\System32\\services.exe",
+            "C:\\Windows\\System32\\lsass.exe",
+            "C:\\Windows\\System32\\csrss.exe",
+            "C:\\Windows\\System32\\smss.exe",
+            "C:\\Windows\\explorer.exe",
+            "C:\\Windows\\System32\\dwm.exe",
+            "C:\\Windows\\System32\\config\\SYSTEM",
+            "C:\\Windows\\System32\\config\\SOFTWARE",
+            "C:\\Windows\\System32\\config\\SAM",
+            "C:\\Windows\\System32\\config\\SECURITY"
+    );
 }
 
